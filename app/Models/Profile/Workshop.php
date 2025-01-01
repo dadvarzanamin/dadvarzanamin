7230a6caa9d4e416743efe2725b7b4ee385831fe
hosseindbk <hosseindbk@yahoo.com>
2025-01-01 14:28:33 +0330

    v2.4.133 workshop 3

--- app/Models/Profile/Workshop.php
+++ app/Models/Profile/Workshop.php
@@ -2,10 +2,22 @@
 
 namespace App\Models\Profile;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Workshop extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => $this->shouldSlug()
+            ]
+        ];
+    }
 }
