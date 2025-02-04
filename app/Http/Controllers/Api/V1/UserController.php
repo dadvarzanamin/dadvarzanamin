a039bb6c4077aa45f5dd78d5bea43f2de8283400
hosseindbk <hosseindbk@yahoo.com>
2025-02-04 20:45:08 +0330

    v2.4.159 auth ssss

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -266,6 +266,7 @@ class UserController extends Controller
     }
 
     public function form(Request $request){
+        dd($request->input()->all);
         if($request->input('type')== 'judgement'){
 
             // ذخیره داده‌ها در پایگاه داده
