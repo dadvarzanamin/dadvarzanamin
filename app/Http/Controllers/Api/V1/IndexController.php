938f703205d00cf0652c7d0784578ab35444b8ab
hosseindbk <hosseindbk@yahoo.com>
2025-02-15 21:01:26 +0330

    v2.4.166 workshop api 1

--- app/Http/Controllers/Api/V1/IndexController.php
+++ app/Http/Controllers/Api/V1/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Emploee;
+use App\Models\Profile\Workshop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
@@ -16,5 +17,35 @@ class IndexController extends Controller
         ];
         return Response::json(['ok' =>true ,'message' => 'success','response'=>$response]);
 
+    }
+    public function workshops(){
+        $workshops = Workshop::select('id', 'title', 'teacher', 'teacher_image', 'teacher_resume', 'price', 'certificate_price', 'offer', 'duration', 'type', 'image', 'video', 'date', 'description', 'target', 'level' , 'status')->get();
+
+        $response = [
+            'workshops' => $workshops->map(function ($workshop) {
+                return [
+                    'id'                => $workshop->id,
+                    'title'             => $workshop->title,
+                    'teacher'           => $workshop->teacher,
+                    'teacher_image'     => $workshop->teacher_image,
+                    'teacher_resume'    => $workshop->teacher_resume,
+                    'price'             => $workshop->price,
+                    'certificate_price' => $workshop->certificate_price,
+                    'offer'             => $workshop->offer,
+                    'duration'          => $workshop->duration,
+                    'type'              => json_decode($workshop->type),
+                    'image'             => $workshop->image,
+                    'video'             => $workshop->video,
+                    'date'              => $workshop->date,
+                    'description'       => $workshop->description,
+//                    'target'            => strip_tags($workshop->target),
+                    'target'            => $workshop->target,
+                    'status'            => $workshop->status,
+                    'level'             => $workshop->level,
+                ];
+            }),
+        ];
+
+        return Response::json(['ok' =>true ,'message' => 'success','response'=>$response]);
     }
 }
