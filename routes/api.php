79a906788ae233b0853614532f6140e0f78c1635
hosseindbk <hosseindbk@yahoo.com>
2025-02-25 19:46:08 +0330

    v2.4.173

--- routes/api.php
+++ routes/api.php
@@ -30,12 +30,14 @@ use App\Http\Controllers\Api\v1\IndexController;
 
 //});
 
+
 Route::middleware('auth:api')->group(function () {
     Route::get('v1/profile'        ,   [App\Http\Controllers\Api\V1\UserController::class       , 'profile']);
     Route::get('v1/demands'        ,   [App\Http\Controllers\Api\V1\UserController::class       , 'demands']);
     Route::get('v1/laws'           ,   [App\Http\Controllers\Api\V1\UserController::class       , 'laws']);
     Route::post('v1/editprofile'   ,   [App\Http\Controllers\Api\V1\UserController::class       , 'editprofile']);
-    Route::post('v1/form'          ,   [App\Http\Controllers\Api\V1\IndexController::class       , 'form']);
+    Route::post('v1/form'          ,   [App\Http\Controllers\Api\V1\IndexController::class      , 'form']);
+    Route::post('v1/discountcheck' ,   [App\Http\Controllers\Api\V1\IndexController::class      , 'discountcheck']);
 
 });
 
