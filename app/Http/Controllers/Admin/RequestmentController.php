d10effec2bf53833659839cf1f5ed5c8b976f141
amir <amirhpz2000@gmail.com>
2025-09-30 20:41:48 +0330

    v2.6.36

--- app/Http/Controllers/Admin/RequestmentController.php
+++ app/Http/Controllers/Admin/RequestmentController.php
@@ -3,85 +3,241 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\APP\judgement;
+use App\Models\APP\legalAdvice;
+use App\Models\APP\tokil;
 use App\Models\Dashboard\Menu_panel;
 use App\Models\Dashboard\Submenu_panel;
 use App\Models\Menu;
 use Illuminate\Http\Request;
-use App\Models\APP\documentDrafting;
-use App\Models\APP\contractDrafting;
-use App\Models\APP\legalAdvice     ;
-use App\Models\APP\judgement       ;
-use App\Models\APP\lawsuit         ;
-use App\Models\APP\tokil           ;
+use Yajra\DataTables\DataTables;
+
 class RequestmentController extends Controller
 {
-        public function index(Request $request)
-        {
-            $thispage       = [
-                'title'         => 'مدیریت منو سایت',
-                'list_title'    => 'لیست منو سایت',
-                'add_title'     => 'افزودن منو سایت',
-                'create_title'  => 'ایجاد منو سایت',
-                'enter_title'   => 'ورود اطلاعات منو سایت',
-            ];
-            $menus          = Menu::all();
-            $menupanels     = Menu_panel::whereStatus(4)->get();
-            $submenupanels  = Submenu_panel::whereStatus(4)->get();
+        public function tokil(Request $request)
+    {
+        $thispage       = [
+            'title'         => 'مدیریت ',
+            'list_title'    => 'لیست ',
+            'add_title'     => 'افزودن ',
+            'create_title'  => 'ایجاد ',
+            'enter_title'   => 'ورود اطلاعات ',
+        ];
+        $menus          = Menu::all();
+        $menupanels     = Menu_panel::whereStatus(4)->get();
+        $submenupanels  = Submenu_panel::whereStatus(4)->get();
 
-
-            $models = [
-                'documentDrafting' => \App\Models\APP\DocumentDrafting::class,
-                'contractDrafting' => \App\Models\APP\ContractDrafting::class,
-                'legalAdvice'      => \App\Models\APP\LegalAdvice::class,
-                'judgement'        => \App\Models\APP\Judgement::class,
-                'lawsuit'          => \App\Models\APP\Lawsuit::class,
-                'tokil'            => \App\Models\APP\Tokil::class,
-            ];
-
-            $data = [];
-
-            foreach ($models as $key => $modelClass) {
-                // اینجا get() روی Query/Model اجرا میشه — نه روی Collection
-                $data[$key] = $modelClass::latest()->get();
-            }
-            dd($data);
-
-            if ($request->ajax()) {
-                return Datatables::of($data)
-                    ->editColumn('title', function ($data) {
-                        return ($data->title);
-                    })
-                    ->editColumn('slug', function ($data) {
-                        return ($data->slug);
-                    })
-                    ->editColumn('priority', function ($data) {
-                        return ($data->priority);
-                    })
-                    ->editColumn('status', function ($data) {
-                        if ($data->status == "0") {
-                            return "عدم نمایش";
-                        } elseif ($data->status == "4") {
-                            return "در حال نمایش";
-                        }
-                    })
-                    ->editColumn('submenu', function ($data) {
-                        if ($data->submenu == 1) {
-                            return "دارد";
-                        } else {
-                            return "ندارد";
-                        }
-                    })
-                    ->addColumn('action', function ($data) {
-                        $actionBtn = '<a href="' . route('menu-site-manage.edit', $data->id) . '" class="btn ripple btn-outline-info btn-icon" style="float: right;margin: 0 5px;"><i class="fe fe-edit-2"></i></a>
+        if ($request->ajax()) {
+            $data = tokil::all();
+            return Datatables::of($data)
+                ->editColumn('case_type', function ($data) {
+                    return ($data->case_type);
+                })
+                ->editColumn('hearing_date', function ($data) {
+                    return ($data->hearing_date);
+                })
+                ->editColumn('court_complex', function ($data) {
+                    return ($data->court_complex);
+                })
+                ->editColumn('court_branch', function ($data) {
+                    return ($data->court_branch);
+                })
+                ->editColumn('additional_info', function ($data) {
+                    return ($data->additional_info);
+                })
+                ->addColumn('action', function ($data) {
+                    $actionBtn = '<a href="' . route('menu-site-manage.edit', $data->id) . '" class="btn ripple btn-outline-info btn-icon" style="float: right;margin: 0 5px;"><i class="fe fe-edit-2"></i></a>
                     <button type="button" id="submit" data-toggle="modal" data-target="#myModal'.$data->id.'" class="btn ripple btn-outline-danger btn-icon " style="float: right;"><i class="fe fe-trash-2 "></i></button>';
 
-                        return $actionBtn;
-                    })
-                    ->rawColumns(['action'])
-                    ->make(true);
-            }
-
-            return view('Admin.requests.all')
-                ->with(compact(['menupanels' , 'submenupanels', 'menus' , 'thispage']));
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
         }
+
+        return view('Admin.tokil.all')
+            ->with(compact(['menupanels' , 'submenupanels', 'menus' , 'thispage']));
+    }
+
+        public function gharardad(Request $request)
+    {
+        $thispage       = [
+            'title'         => 'مدیریت ',
+            'list_title'    => 'لیست ',
+            'add_title'     => 'افزودن ',
+            'create_title'  => 'ایجاد ',
+            'enter_title'   => 'ورود اطلاعات ',
+        ];
+        $menus          = Menu::all();
+        $menupanels     = Menu_panel::whereStatus(4)->get();
+        $submenupanels  = Submenu_panel::whereStatus(4)->get();
+
+        if ($request->ajax()) {
+            $data = ContractDrafting::all();
+            return Datatables::of($data)
+                ->editColumn('contract_type', function ($data) {
+                    return ($data->contract_type);
+                })
+                ->editColumn('party_one_name', function ($data) {
+                    return ($data->party_one_name);
+                })
+                ->editColumn('party_two_name', function ($data) {
+                    return ($data->party_two_name);
+                })
+                ->editColumn('party_one_national_id', function ($data) {
+                    return ($data->party_one_national_id);
+                })
+                ->editColumn('party_two_national_id', function ($data) {
+                    return ($data->party_two_national_id);
+                })
+                ->addColumn('action', function ($data) {
+                    $actionBtn = '<a href="' . route('menu-site-manage.edit', $data->id) . '" class="btn ripple btn-outline-info btn-icon" style="float: right;margin: 0 5px;"><i class="fe fe-edit-2"></i></a>
+                    <button type="button" id="submit" data-toggle="modal" data-target="#myModal'.$data->id.'" class="btn ripple btn-outline-danger btn-icon " style="float: right;"><i class="fe fe-trash-2 "></i></button>';
+
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('Admin.gharardad.all')
+            ->with(compact(['menupanels' , 'submenupanels', 'menus' , 'thispage']));
+    }
+
+        public function parvande(Request $request)
+    {
+        $thispage       = [
+            'title'         => 'مدیریت ',
+            'list_title'    => 'لیست ',
+            'add_title'     => 'افزودن ',
+            'create_title'  => 'ایجاد ',
+            'enter_title'   => 'ورود اطلاعات ',
+        ];
+        $menus          = Menu::all();
+        $menupanels     = Menu_panel::whereStatus(4)->get();
+        $submenupanels  = Submenu_panel::whereStatus(4)->get();
+
+        if ($request->ajax()) {
+            $data = Lawsuit::all();
+            return Datatables::of($data)
+                ->editColumn('case_type', function ($data) {
+                    return ($data->case_type);
+                })
+                ->editColumn('case_subject', function ($data) {
+                    return ($data->case_subject);
+                })
+                ->editColumn('stage', function ($data) {
+                    return ($data->stage);
+                })
+                ->editColumn('opponent_name', function ($data) {
+                    return ($data->opponent_name);
+                })
+                ->editColumn('opponent_national_id', function ($data) {
+                    return ($data->opponent_national_id);
+                })
+                ->addColumn('action', function ($data) {
+                    $actionBtn = '<a href="' . route('menu-site-manage.edit', $data->id) . '" class="btn ripple btn-outline-info btn-icon" style="float: right;margin: 0 5px;"><i class="fe fe-edit-2"></i></a>
+                    <button type="button" id="submit" data-toggle="modal" data-target="#myModal'.$data->id.'" class="btn ripple btn-outline-danger btn-icon " style="float: right;"><i class="fe fe-trash-2 "></i></button>';
+
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('Admin.parvande.all')
+            ->with(compact(['menupanels' , 'submenupanels', 'menus' , 'thispage']));
+    }
+
+        public function moshavere(Request $request)
+    {
+        $thispage       = [
+            'title'         => 'مدیریت ',
+            'list_title'    => 'لیست ',
+            'add_title'     => 'افزودن ',
+            'create_title'  => 'ایجاد ',
+            'enter_title'   => 'ورود اطلاعات ',
+        ];
+        $menus          = Menu::all();
+        $menupanels     = Menu_panel::whereStatus(4)->get();
+        $submenupanels  = Submenu_panel::whereStatus(4)->get();
+
+        if ($request->ajax()) {
+            $data = legalAdvice::all();
+            return Datatables::of($data)
+                ->editColumn('topic', function ($data) {
+                    return ($data->topic);
+                })
+                ->editColumn('sub_topic', function ($data) {
+                    return ($data->sub_topic);
+                })
+                ->editColumn('type', function ($data) {
+                    return ($data->type);
+                })
+                ->editColumn('additional_info', function ($data) {
+                    return ($data->additional_info);
+                })
+
+                ->addColumn('action', function ($data) {
+                    $actionBtn = '<a href="' . route('menu-site-manage.edit', $data->id) . '" class="btn ripple btn-outline-info btn-icon" style="float: right;margin: 0 5px;"><i class="fe fe-edit-2"></i></a>
+                    <button type="button" id="submit" data-toggle="modal" data-target="#myModal'.$data->id.'" class="btn ripple btn-outline-danger btn-icon " style="float: right;"><i class="fe fe-trash-2 "></i></button>';
+
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('Admin.moshavere.all')
+            ->with(compact(['menupanels' , 'submenupanels', 'menus' , 'thispage']));
+    }
+
+        public function davari(Request $request)
+    {
+        $thispage       = [
+            'title'         => 'مدیریت ',
+            'list_title'    => 'لیست ',
+            'add_title'     => 'افزودن ',
+            'create_title'  => 'ایجاد ',
+            'enter_title'   => 'ورود اطلاعات ',
+        ];
+        $menus          = Menu::all();
+        $menupanels     = Menu_panel::whereStatus(4)->get();
+        $submenupanels  = Submenu_panel::whereStatus(4)->get();
+
+        if ($request->ajax()) {
+            $data = judgement::all();
+            return Datatables::of($data)
+                ->editColumn('judgement_type', function ($data) {
+                    return ($data->judgement_type);
+                })
+                ->editColumn('contract_type', function ($data) {
+                    return ($data->contract_type);
+                })
+                ->editColumn('party_one_name', function ($data) {
+                    return ($data->party_one_name);
+                })
+                ->editColumn('party_two_name', function ($data) {
+                    return ($data->party_two_name);
+                })
+                ->editColumn('party_one_national_id', function ($data) {
+                    return ($data->party_one_national_id);
+                })
+                ->editColumn('party_two_national_id', function ($data) {
+                    return ($data->party_two_national_id);
+                })
+                ->addColumn('action', function ($data) {
+                    $actionBtn = '<a href="' . route('menu-site-manage.edit', $data->id) . '" class="btn ripple btn-outline-info btn-icon" style="float: right;margin: 0 5px;"><i class="fe fe-edit-2"></i></a>
+                    <button type="button" id="submit" data-toggle="modal" data-target="#myModal'.$data->id.'" class="btn ripple btn-outline-danger btn-icon " style="float: right;"><i class="fe fe-trash-2 "></i></button>';
+
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('Admin.davari.all')
+            ->with(compact(['menupanels' , 'submenupanels', 'menus' , 'thispage']));
+    }
+
 }
